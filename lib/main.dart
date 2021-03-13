ec68e872af32786b6c0b17a700e90acc96d3e136
MoryokaV <mariovlaviano2005@yahoo.com>
2021-03-13 16:31:35 +0200

    revert last commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() {
 }
 
 void test() async{
-  var url = "http://10.0.2.2:5000/";
+  var url = "http://127.0.0.1:5000/";
 
   Response response = await get(Uri.parse(url + "find?productID=1"));
   print(response.body);
